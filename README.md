c83934d8689adcf4d6573843e39b0fe03d3d5d35
TakeAsh <take@TakeAsh.net>
2020-05-24 07:09:48 +0900

    initialized by Minilla

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Net::Recorder - It's new $module
+
+# SYNOPSIS
+
+    use Net::Recorder;
+
+# DESCRIPTION
+
+Net::Recorder is ...
+
+# LICENSE
+
+Copyright (C) TakeAsh.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+TakeAsh &lt;take@TakeAsh.net>
---
+++ lib/Net/Recorder.pm
@@ -0,0 +1,39 @@
+package Net::Recorder;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::Recorder - It's new $module
+
+=head1 SYNOPSIS
+
+    use Net::Recorder;
+
+=head1 DESCRIPTION
+
+Net::Recorder is ...
+
+=head1 LICENSE
+
+Copyright (C) TakeAsh.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+TakeAsh E<lt>take@TakeAsh.netE<gt>
+
+=cut
+
