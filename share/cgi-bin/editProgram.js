3a1f6cf57f14ffa44b353677302fb3da06b417c6
TakeAsh <take@TakeAsh.net>
2025-01-05 18:34:09 +0900

    editProgram: add erase buttons

--- share/cgi-bin/editProgram.js
+++ share/cgi-bin/editProgram.js
@@ -1,8 +1,24 @@
 ﻿const d = document;
 
-d.getElementById('buttonEraseId').addEventListener(
-  'click',
-  (event) => {
-    d.getElementById('inputID').value = '';
-  }
-);
+[
+  [
+    'buttonEraseId',
+    (event) => { d.getElementById('inputID').value = ''; }
+  ],
+  [
+    'buttonEraseExtra',
+    (event) => {
+      const textArea = d.getElementById('textareaExtra');
+      textArea.value = textArea.value.replace(/([^=\n]+)=([^\n]*)/g, '$1=');
+    }
+  ],
+  [
+    'buttonEraseProperties',
+    (event) => {
+      Array.from(event.target.parentNode.parentNode.querySelectorAll('textarea, input[type="text"]'))
+        .forEach(input => { input.value = ''; });
+    }
+  ],
+].forEach(button => {
+  d.getElementById(button[0]).addEventListener('click', button[1]);
+});
